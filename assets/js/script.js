1fb7f10650f22ce857923d92a639c628d21f975b
Kalefoo <kalefoo007@gmail.com>
2022-07-09 13:17:46 -0300

    Desafío terminado ;)

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,36 +7,41 @@ submitButton.addEventListener('click', async () => {
         mode: 'no-cors'
     }).then(response => response.json()).then(json => json).catch(err => console.log('Solicitud fallida', err))
 
-    const info = document.getElementById('info')
+    const info = document.getElementById('viewInfo')
 
     var DateTime = luxon.DateTime
     const time = DateTime.now().setZone(infoIP.timezone || 'UTC')
 
-    const divIP = `<div id="info">
-        <article class="containerInfo">
-            <h3>IP: ${infoIP.query || 'Sin datos'}</h3>
-            <h3>AS: ${infoIP.as || 'Sin datos'}</h3>
-            <h3>Nombre AS: ${infoIP.asname || 'Sin datos'}</h3>
-            <h3>País: ${infoIP.country || 'Sin datos'} (${infoIP.countryCode || 'Sin datos'})</h3>
-            <h3>Continente: ${infoIP.continent || 'Sin datos'} (${infoIP.continentCode || 'Sin datos'})</h3>
-            <h3>Región: ${infoIP.regionName || 'Sin datos'} (${infoIP.region || 'Sin datos'})</h3>
-            <h3>Ciudad: ${infoIP.city || 'Sin datos'}</h3>
-            <h3>Código postal: ${infoIP.zip || 'Sin datos'}</h3>
-            <h3>Latitud: ${infoIP.lat || 'Sin datos'}</h3>
-            <h3>Longitud: ${infoIP.lon || 'Sin datos'}</h3>
-            <h3>Zona horaria: ${infoIP.timezone || 'Sin datos'} (${time.toLocaleString(DateTime.TIME_24_SIMPLE)})</h3>
-            <h3>Moneda: ${infoIP.currency || 'Sin datos'}</h3>
-            <h3>Proveedor: ${infoIP.org || 'Sin datos'}</h3>
-        </article>
-        <article class="containerInfo">
-            <div id='map' class="map"></div>
-        </article>
-    </div>`
+    const divInfo = 
+        `<div id="viewInfo">
+            <h2>Información</h2>
+            
+            <div id="info">
+                <article class="containerInfo">
+                    <h3>IP: ${infoIP.query || 'Sin datos'}</h3>
+                    <h3>AS: ${infoIP.as || 'Sin datos'}</h3>
+                    <h3>Nombre AS: ${infoIP.asname || 'Sin datos'}</h3>
+                    <h3>País: ${infoIP.country || 'Sin datos'} (${infoIP.countryCode || 'Sin datos'})</h3>
+                    <h3>Continente: ${infoIP.continent || 'Sin datos'} (${infoIP.continentCode || 'Sin datos'})</h3>
+                    <h3>Región: ${infoIP.regionName || 'Sin datos'} (${infoIP.region || 'Sin datos'})</h3>
+                    <h3>Ciudad: ${infoIP.city || 'Sin datos'}</h3>
+                    <h3>Código postal: ${infoIP.zip || 'Sin datos'}</h3>
+                    <h3>Latitud: ${infoIP.lat || 'Sin datos'}</h3>
+                    <h3>Longitud: ${infoIP.lon || 'Sin datos'}</h3>
+                    <h3>Zona horaria: ${infoIP.timezone || 'Sin datos'} (${time.toLocaleString(DateTime.TIME_24_SIMPLE)})</h3>
+                    <h3>Moneda: ${infoIP.currency || 'Sin datos'}</h3>
+                    <h3>Proveedor: ${infoIP.org || 'Sin datos'}</h3>
+                </article>
+                <article class="containerInfo">
+                    <div id='map' class="map"></div>
+                </article>
+            </div>
+        </div>`
 
-    info.innerHTML = divIP
+    info.innerHTML = divInfo
 
     
-    mapboxgl.accessToken = 'pk.eyJ1IjoibGF1dHlkZXYiLCJhIjoiY2w1Y3hpMnpuMGx6bjNpczBiMDdqb2h6bSJ9.GJt3QOsLYQ8gwAhYL6WsSg';
+    mapboxgl.accessToken = 'pk.eyJ1IjoibGF1dHlkZXYiLCJhIjoiY2w1Y3htOHJ5MGVpbjNibjN3MGQxbjV0NSJ9.vgg471uOM8q8EM9vpX3CgQ';
     
     const geojson = {
         'type': 'FeatureCollection',
